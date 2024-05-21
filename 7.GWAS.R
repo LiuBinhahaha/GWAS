9fc9e473e3aef5ffc8afa775ebd8240b35f89f76
LiuBinhahaha <124219920+LiuBinhahaha@users.noreply.github.com>
2024-05-21 08:50:44 +0800

    Update 7.GWAS.R

--- 7.GWAS.R
+++ 7.GWAS.R
@@ -7,3 +7,12 @@ head(pmap)
 threshold <- 1/nrow(pmap[!is.na(pmap$pos),])
 # 画图
 CMplot(pmap, threshold = threshold, amplify = F, file = "tiff", plot.type=c("m","q"))
+
+plot.type="m"：曼哈顿图
+plot.type：可以选择 "d", "c", "m", "q" or "b"
+plot.type="d", SNP密度图
+plot.type="c", 环形曼哈顿图
+plot.type="m",曼哈顿图
+plot.type="q",QQ图
+plot.type="b",同时绘制环形曼哈顿图、曼哈顿图和QQ图
+plot.type=c("m","q"), 绘制曼哈顿图和QQ图
