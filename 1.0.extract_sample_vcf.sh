d01e869f140696073925ab70a10042a7f4c0021e
LiuBinhahaha <124219920+LiuBinhahaha@users.noreply.github.com>
2024-10-07 17:24:09 +0800

    Update 1.0.extract_sample_vcf.sh

--- 1.0.extract_sample_vcf.sh
+++ 1.0.extract_sample_vcf.sh
@@ -5,7 +5,7 @@ bcftools index 1844_0.05_0.1_snps_ID.vcf.gz
 bcftools view 1844_0.05_0.1_snps_ID.vcf.gz -S 499.sample > 499_sample.vcf
 
 # parallel并行提取多个小群体的vcf文件：
-sample=$(ls *.sample | awk -F '.' '{print $1}'); parallel -j 'echo {} && bcftools view 1844_0.05_0.1_snps_ID.vcf.gz -S {} > {}.vcf' ::: "${sample[@]}"
+sample=$(ls *.sample | awk -F '.' '{print $1}'); parallel -j 10 'echo {} && bcftools view 1844_0.05_0.1_snps_ID.vcf.gz -S {} > {}.vcf' ::: "${sample[@]}"
 
 # 集群批量提取
 准备提取模板：echo "bcftools view 1844_0.05_0.1_snps_ID.vcf.gz -S xxx > xxx.vcf" > extract_vcf.sh
